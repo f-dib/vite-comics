38e938f21e044a0f19396951507847a2b94d17c7
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-29 19:03:33 +0100

    added style on appcontent

--- src/components/main/AppContent.vue
+++ src/components/main/AppContent.vue
@@ -12,10 +12,18 @@ export default {
 </script>
 
 <template>
-
+    <main>
+        <div class="my_width my_height">
+            -> Content goes Here 
+        </div>
+    </main>
     <InfoBar></InfoBar>
 </template>
 
 <style lang="scss">
-
+    .my_height {
+        padding: 50px 5px;
+        font-weight: bold;
+        font-size: 20px;
+    }
 </style>
